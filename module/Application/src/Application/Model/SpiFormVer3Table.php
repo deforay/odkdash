0d23fe5ecdc69298e773ecc5d3c2a7813ca76b6a
Gunaseelan <guna@deforay.com>
2016-04-29 17:49:13 +0530

    pending to approved spi form updated.

--- module/Application/src/Application/Model/SpiFormVer3Table.php
+++ module/Application/src/Application/Model/SpiFormVer3Table.php
@@ -592,6 +592,154 @@ class SpiFormVer3Table extends AbstractTableGateway {
        }
        return $output;
     }
+    
+    public function fetchAllSubmissionsDatas($parameters)
+    {
+         /* Array of database columns which should be read and sent back to DataTables. Use a space where
+        * you want to insert a non-database field (for example a counter or static image)
+        */
+	
+        $aColumns = array('facilityname','formId', 'formVersion','auditroundno','assesmentofaudit' ,'testingpointtype','level','affiliation','AUDIT_SCORE_PERCANTAGE','status');
+        $orderColumns = array('facilityname','formId', 'formVersion','auditroundno','assesmentofaudit' ,'testingpointtype','level','affiliation','AUDIT_SCORE_PERCANTAGE','status');
+
+       /*
+        * Paging
+        */
+       $sLimit = "";
+       if (isset($parameters['iDisplayStart']) && $parameters['iDisplayLength'] != '-1') {
+           $sOffset = $parameters['iDisplayStart'];
+           $sLimit = $parameters['iDisplayLength'];
+       }
+
+       /*
+        * Ordering
+        */
+
+       $sOrder = "";
+       if (isset($parameters['iSortCol_0'])) {
+           for ($i = 0; $i < intval($parameters['iSortingCols']); $i++) {
+               if ($parameters['bSortable_' . intval($parameters['iSortCol_' . $i])] == "true") {
+                   $sOrder .= $orderColumns[intval($parameters['iSortCol_' . $i])] . " " . ( $parameters['sSortDir_' . $i] ) . ",";
+               }
+           }
+           $sOrder = substr_replace($sOrder, "", -1);
+       }
+
+       /*
+        * Filtering
+        * NOTE this does not match the built-in DataTables filtering which does it
+        * word by word on any field. It's possible to do here, but concerned about efficiency
+        * on very large tables, and MySQL's regex functionality is very limited
+        */
+
+       $sWhere = "";
+       if (isset($parameters['sSearch']) && $parameters['sSearch'] != "") {
+           $searchArray = explode(" ", $parameters['sSearch']);
+           $sWhereSub = "";
+           foreach ($searchArray as $search) {
+               if ($sWhereSub == "") {
+                   $sWhereSub .= "(";
+               } else {
+                   $sWhereSub .= " AND (";
+               }
+               $colSize = count($aColumns);
+
+               for ($i = 0; $i < $colSize; $i++) {
+                   if ($i < $colSize - 1) {
+                       $sWhereSub .= $aColumns[$i] . " LIKE '%" . ($search ) . "%' OR ";
+                   } else {
+                       $sWhereSub .= $aColumns[$i] . " LIKE '%" . ($search ) . "%' ";
+                   }
+               }
+               $sWhereSub .= ")";
+           }
+           $sWhere .= $sWhereSub;
+       }
+
+       /* Individual column filtering */
+       for ($i = 0; $i < count($aColumns); $i++) {
+           if (isset($parameters['bSearchable_' . $i]) && $parameters['bSearchable_' . $i] == "true" && $parameters['sSearch_' . $i] != '') {
+               if ($sWhere == "") {
+                   $sWhere .= $aColumns[$i] . " LIKE '%" . ($parameters['sSearch_' . $i]) . "%' ";
+               } else {
+                   $sWhere .= " AND " . $aColumns[$i] . " LIKE '%" . ($parameters['sSearch_' . $i]) . "%' ";
+               }
+           }
+       }
+
+       /*
+        * SQL queries
+        * Get data to display
+        */
+       $dbAdapter = $this->adapter;
+       $sql = new Sql($dbAdapter);
+       $sQuery = $sql->select()->from(array('spiv3' => 'spi_form_v_3'));
+       
+       if (isset($sWhere) && $sWhere != "") {
+           $sQuery->where($sWhere);
+       }
+
+       if (isset($sOrder) && $sOrder != "") {
+           $sQuery->order($sOrder);
+       }
+
+       if (isset($sLimit) && isset($sOffset)) {
+           $sQuery->limit($sLimit);
+           $sQuery->offset($sOffset);
+       }
+
+       $sQueryStr = $sql->getSqlStringForSqlObject($sQuery); // Get the string of the Sql, instead of the Select-instance 
+       //echo $sQueryStr;die;
+       $rResult = $dbAdapter->query($sQueryStr, $dbAdapter::QUERY_MODE_EXECUTE);
+
+       /* Data set length after filtering */
+       $sQuery->reset('limit');
+       $sQuery->reset('offset');
+       $fQuery = $sql->getSqlStringForSqlObject($sQuery);
+       $aResultFilterTotal = $dbAdapter->query($fQuery, $dbAdapter::QUERY_MODE_EXECUTE);
+       $iFilteredTotal = count($aResultFilterTotal);
+
+       /* Total data set length */
+	$tQuery =  $sql->select()->from(array('spiv3' => 'spi_form_v_3'));
+	$tQueryStr = $sql->getSqlStringForSqlObject($tQuery); // Get the string of the Sql, instead of the Select-instance
+	$tResult = $dbAdapter->query($tQueryStr, $dbAdapter::QUERY_MODE_EXECUTE);
+	$iTotal = count($tResult);
+	$output = array(
+           "sEcho" => intval($parameters['sEcho']),
+           "iTotalRecords" => $iTotal,
+           "iTotalDisplayRecords" => $iFilteredTotal,
+           "aaData" => array()
+	);
+	
+       $commonService = new \Application\Service\CommonService();
+       foreach ($rResult as $aRow) {
+        $row = array();
+        $row['DT_RowId'] = $aRow['id'];
+        $row[] = $aRow['facilityname'];
+        $row[] = $aRow['formId'];
+        $row[] = $aRow['formVersion'];
+        $row[] = $aRow['auditroundno'];
+        $row[] = $commonService->humanDateFormat($aRow['assesmentofaudit']);
+        $row[] = $aRow['testingpointtype'];
+        $row[] = $aRow['level'];
+        $row[] = $aRow['affiliation'];
+        $row[] = round($aRow['AUDIT_SCORE_PERCANTAGE'],2);
+        $row[] = ucwords($aRow['status']);
+        $output['aaData'][] = $row;
+       }
+       return $output;
+    }
+    
+    public function fetchPendingFacilityNames()
+    {
+        $dbAdapter = $this->adapter;
+        $sql = new Sql($dbAdapter);
+        $sQuery = $sql->select()->from(array('spiv3' => 'spi_form_v_3'))
+                                ->where(array('spiv3.status'=>'pending'));
+        $sQueryStr = $sql->getSqlStringForSqlObject($sQuery);
+        $rResult = $dbAdapter->query($sQueryStr, $dbAdapter::QUERY_MODE_EXECUTE)->toArray();
+        return $rResult;
+    }
 
     public function getFormData($id) {
         $row = $this->select(array('id' => (int) $id))->current();
