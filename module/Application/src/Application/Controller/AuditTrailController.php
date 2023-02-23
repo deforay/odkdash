867fcb3ac93ef8061552d1635c01158fa7b5c12a
Jeyabanu <jeyabanu@deforay.com>
2023-02-23 12:28:55 +0530

    Monir changes done in audit trail

--- module/Application/src/Application/Controller/AuditTrailController.php
+++ module/Application/src/Application/Controller/AuditTrailController.php
@@ -23,16 +23,25 @@ class AuditTrailController extends AbstractActionController
     {
         /** @var \Laminas\Http\Request $request */
         $request = $this->getRequest();
+          
         if ($request->isPost()) {
             $params = $request->getPost();
-                if(!empty($params['version']) && $params['version']=='v3')
-                $result = $this->auditTrailService->getSpiV3Details($params);
-                    else
-                $result = $this->auditTrailService->getSpiV6Details($params);
+            $colCount = "0";
+              if(!empty($params['version']) && $params['version']=='v3')
+              {
+                  $result = $this->auditTrailService->getSpiV3Details($params);
+                  $colCount = 239;
+              }
+              else
+              {
+                  $result = $this->auditTrailService->getSpiV6Details($params);
+                  $colCount = 320;
+              }
             //return $this->getResponse()->setContent(Json::encode($result));
             return new ViewModel(array(
                 'result' => $result,
                 'params' => $params,
+                'colCount' => $colCount,
             ));
         }
 
