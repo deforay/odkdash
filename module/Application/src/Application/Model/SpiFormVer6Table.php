1aff1f973696e9dc5a6d0f6accd9ea5e33f2115b
Amit Dugar <amit@deforay.com>
2023-05-26 18:10:00 +0530

    Minor fix in SPI v6 Data Parser

--- module/Application/src/Application/Model/SpiFormVer6Table.php
+++ module/Application/src/Application/Model/SpiFormVer6Table.php
@@ -72,7 +72,7 @@ class SpiFormVer6Table extends AbstractTableGateway
             $approveStatus = 'pending';
         }
 
-        //error_log(json_encode($params,true));
+        error_log(json_encode($params, true));
 
         foreach ($params['data'] as $datar) {
             $par = array();
@@ -426,7 +426,7 @@ class SpiFormVer6Table extends AbstractTableGateway
         }
     }
 
-    public function saveOdkCentralData($params, $formDetails, $correctiveActions,$projectId,$formId)
+    public function saveOdkCentralData($params, $formDetails, $correctiveActions, $projectId, $formId)
     {
         // print_r($formDetails);die;
         if ($params == null || $params == "" || (is_array($params) && count($params['value']) == 0)) {
@@ -834,26 +834,26 @@ class SpiFormVer6Table extends AbstractTableGateway
                         'name_auditor2' => $data['nameOfAuditor2'] ?? null,
                         'info4' => $data['info4'],
                         'INSTANCE' => $data['INSTANCE'],
-                        'PERSONAL_Q_1_1_HIV_TRAINING' => $data['PERSONAL_Q_1_1_HIV_TRAINING'],
-                        'PERSONAL_C_1_1_HIV_TRAINING' => $data['PERSONAL_C_1_1_HIV_TRAINING'],
-                        'PERSONAL_Q_1_2_HIV_TESTING_REGISTER' => $data['PERSONAL_Q_1_2_HIV_TESTING_REGISTER'],
-                        'PERSONAL_C_1_2_HIV_TESTING_REGISTER' => $data['PERSONAL_C_1_2_HIV_TESTING_REGISTER'],
-                        'PERSONAL_Q_1_3_EQA_PT' => $data['PERSONAL_Q_1_3_EQA_PT'],
-                        'PERSONAL_C_1_3_EQA_PT' => $data['PERSONAL_C_1_3_EQA_PT'],
-                        'PERSONAL_Q_1_4_QC_PROCESS' => $data['PERSONAL_Q_1_4_QC_PROCESS'],
-                        'PERSONAL_C_1_4_QC_PROCESS' => $data['PERSONAL_C_1_4_QC_PROCESS'],
-                        'PERSONAL_Q_1_5_SAFETY_MANAGEMENT' => $data['PERSONAL_Q_1_5_SAFETY_MANAGEMENT'],
-                        'PERSONAL_C_1_5_SAFETY_MANAGEMENT' => $data['PERSONAL_C_1_5_SAFETY_MANAGEMENT'],
-                        'PERSONAL_Q_1_6_REFRESHER_TRAINING' => $data['PERSONAL_Q_1_6_REFRESHER_TRAINING'],
-                        'PERSONAL_C_1_6_REFRESHER_TRAINING' => $data['PERSONAL_C_1_6_REFRESHER_TRAINING'],
-                        'PERSONAL_Q_1_7_HIV_COMPETENCY_TESTING' => $data['PERSONAL_Q_1_7_HIV_COMPETENCY_TESTING'],
-                        'PERSONAL_C_1_7_HIV_COMPETENCY_TESTING' => $data['PERSONAL_C_1_7_HIV_COMPETENCY_TESTING'],
-                        'PERSONAL_Q_1_8_NATIONAL_CERTIFICATION' => $data['PERSONAL_Q_1_8_NATIONAL_CERTIFICATION'],
-                        'PERSONAL_C_1_8_NATIONAL_CERTIFICATION' => $data['PERSONAL_C_1_8_NATIONAL_CERTIFICATION'],
-                        'PERSONAL_Q_1_9_CERTIFIED_TESTERS' => $data['PERSONAL_Q_1_9_CERTIFIED_TESTERS'],
-                        'PERSONAL_C_1_9_CERTIFIED_TESTERS' => $data['PERSONAL_C_1_9_CERTIFIED_TESTERS'],
-                        'PERSONAL_Q_1_10_RECERTIFIED' => $data['PERSONAL_Q_1_10_RECERTIFIED'],
-                        'PERSONAL_C_1_10_RECERTIFIED' => $data['PERSONAL_C_1_10_RECERTIFIED'],
+                        'PERSONAL_Q_1_1_HIV_TRAINING' => $data['PERSONAL_Q_1_1_HIV_TRAINING'] ?? $data['PERSONAL_Q_1_1'] ?? null,
+                        'PERSONAL_C_1_1_HIV_TRAINING' => $data['PERSONAL_C_1_1_HIV_TRAINING'] ?? $data['PERSONAL_C_1_1'] ?? null,
+                        'PERSONAL_Q_1_2_HIV_TESTING_REGISTER' => $data['PERSONAL_Q_1_2_HIV_TESTING_REGISTER'] ?? $data['PERSONAL_Q_1_2'] ?? null,
+                        'PERSONAL_C_1_2_HIV_TESTING_REGISTER' => $data['PERSONAL_C_1_2_HIV_TESTING_REGISTER'] ?? $data['PERSONAL_C_1_2'] ?? null,
+                        'PERSONAL_Q_1_3_EQA_PT' => $data['PERSONAL_Q_1_3_EQA_PT'] ?? $data['PERSONAL_Q_1_3'] ?? null,
+                        'PERSONAL_C_1_3_EQA_PT' => $data['PERSONAL_C_1_3_EQA_PT'] ?? $data['PERSONAL_C_1_3'] ?? null,
+                        'PERSONAL_Q_1_4_QC_PROCESS' => $data['PERSONAL_Q_1_4_QC_PROCESS'] ?? $data['PERSONAL_Q_1_4'] ?? null,
+                        'PERSONAL_C_1_4_QC_PROCESS' => $data['PERSONAL_C_1_4_QC_PROCESS'] ?? $data['PERSONAL_C_1_4'] ?? null,
+                        'PERSONAL_Q_1_5_SAFETY_MANAGEMENT' => $data['PERSONAL_Q_1_5_SAFETY_MANAGEMENT'] ?? $data['PERSONAL_Q_1_5'] ?? null,
+                        'PERSONAL_C_1_5_SAFETY_MANAGEMENT' => $data['PERSONAL_C_1_5_SAFETY_MANAGEMENT'] ?? $data['PERSONAL_C_1_5'] ?? null,
+                        'PERSONAL_Q_1_6_REFRESHER_TRAINING' => $data['PERSONAL_Q_1_6_REFRESHER_TRAINING'] ?? $data['PERSONAL_Q_1_6'] ?? null,
+                        'PERSONAL_C_1_6_REFRESHER_TRAINING' => $data['PERSONAL_C_1_6_REFRESHER_TRAINING'] ?? $data['PERSONAL_C_1_6'] ?? null,
+                        'PERSONAL_Q_1_7_HIV_COMPETENCY_TESTING' => $data['PERSONAL_Q_1_7_HIV_COMPETENCY_TESTING'] ?? $data['PERSONAL_Q_1_7'] ?? null,
+                        'PERSONAL_C_1_7_HIV_COMPETENCY_TESTING' => $data['PERSONAL_C_1_7_HIV_COMPETENCY_TESTING'] ?? $data['PERSONAL_C_1_7'] ?? null,
+                        'PERSONAL_Q_1_8_NATIONAL_CERTIFICATION' => $data['PERSONAL_Q_1_8_NATIONAL_CERTIFICATION'] ?? $data['PERSONAL_Q_1_8'] ?? null,
+                        'PERSONAL_C_1_8_NATIONAL_CERTIFICATION' => $data['PERSONAL_C_1_8_NATIONAL_CERTIFICATION'] ?? $data['PERSONAL_C_1_8'] ?? null,
+                        'PERSONAL_Q_1_9_CERTIFIED_TESTERS' => $data['PERSONAL_Q_1_9_CERTIFIED_TESTERS'] ?? $data['PERSONAL_Q_1_9'] ?? null,
+                        'PERSONAL_C_1_9_CERTIFIED_TESTERS' => $data['PERSONAL_C_1_9_CERTIFIED_TESTERS'] ?? $data['PERSONAL_C_1_9'] ?? null,
+                        'PERSONAL_Q_1_10_RECERTIFIED' => $data['PERSONAL_Q_1_10_RECERTIFIED'] ?? $data['PERSONAL_Q_1_10'] ?? null,
+                        'PERSONAL_C_1_10_RECERTIFIED' => $data['PERSONAL_C_1_10_RECERTIFIED'] ?? $data['PERSONAL_C_1_10'] ?? null,
                         'PERSONAL_SCORE' => $data['PERSONAL_SCORE'],
                         'PERSONAL_Display' => $data['PERSONAL_Display'],
                         'PERSONALPHOTO' => $data['PERSONALPHOTO'],
@@ -3295,7 +3295,7 @@ class SpiFormVer6Table extends AbstractTableGateway
             $result = $this->update($data, array('id' => $formId));
             $subject = '';
             $eventType = 'Update-SPI RT Form 6-Request';
-            $action = $user_name . ' has updated the SPI RT Form 6 information '.$formId;
+            $action = $user_name . ' has updated the SPI RT Form 6 information ' . $formId;
             $resourceName = 'SPI-RT-Form-6';
             $eventTable->addEventLog($subject, $eventType, $action, $resourceName);
 
@@ -3401,7 +3401,7 @@ class SpiFormVer6Table extends AbstractTableGateway
         $dbAdapter = $this->adapter;
         $sql = new Sql($this->adapter);
         $uQuery = $sql->select()->from(array('spiv5' => 'spi_form_v_6'))->columns(array('facilityname' => new Expression("DISTINCT facilityname")))
-        ->order('spiv5.facilityname ASC');
+            ->order('spiv5.facilityname ASC');
         $uQueryStr = $sql->buildSqlString($uQuery);
         $uResult = $dbAdapter->query($uQueryStr, $dbAdapter::QUERY_MODE_EXECUTE)->toArray();
 
@@ -3621,7 +3621,7 @@ class SpiFormVer6Table extends AbstractTableGateway
             $result = $this->update($data, array('id' => $params['deleteId']));
             $subject = '';
             $eventType = 'Delete-SPI RT Form 6';
-            $action = $user_name . ' has deleted the SPI RT Form 6 information '.$params['deleteId'];
+            $action = $user_name . ' has deleted the SPI RT Form 6 information ' . $params['deleteId'];
             $resourceName = 'SPI-RT-Form-6';
             $eventTable->addEventLog($subject, $eventType, $action, $resourceName);
         }
@@ -5281,7 +5281,7 @@ class SpiFormVer6Table extends AbstractTableGateway
         return $id;
     }
 
-    public function getLatestFormDate($projectId,$formId)
+    public function getLatestFormDate($projectId, $formId)
     {
         $dbAdapter = $this->adapter;
         $sql = new Sql($this->adapter);
