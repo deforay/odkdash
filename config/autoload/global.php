ea40c4f957c6a7e073051ef5120f7c62c01259d7
Thanaseelan <thanaseelan@deforay.com>
2024-09-06 16:50:18 +0530

    Added Symfony mailer and libraries

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -15,7 +15,7 @@
 return array(
     'db' => array(
         'driver'         => 'Pdo',
-        'dsn'            => 'mysql:dbname=odkdash;host=localhost',
+        'dsn'            => 'mysql:dbname=odkdash-jamaica;host=localhost',
         'driver_options' => array(
             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
         ),
